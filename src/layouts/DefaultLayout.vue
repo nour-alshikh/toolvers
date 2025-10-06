b0c457fc91bcf11e1c33c2a026c0e0326faba4f7
nour alshekh <nalshekh@reeras.local>
2025-10-06 14:52:23 +0300

    footer

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -1,6 +1,7 @@
   
   <script setup lang="ts">
   import Header from '@/views/components/Header.vue'
+  import Footer from '@/views/components/Footer.vue'
   </script>
   
 <template>
@@ -10,6 +11,7 @@
   <Header />
   <div class=" py-4 px-2 lg:px-8">
     <slot />
+    <Footer />
   </div>
 </div>
 </template>
---
+++ src/views/components/Footer.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <footer class="bg-secondaryBackground mt-2 lg:py-5 py-2 text-xs rounded-lg lg:px-8 px-2 flex justify-center items-center">
+      <p class="text-center mb-0">
+        Copyright © 
+        <span class="text-primary">
+          Toolvers
+        </span>
+        
+        2025</p>
+  </footer>
+</template>
